879df6bc57c91935f4a7256257befd6d49a02620
Nha Nguyen <nguyenphucnha111@gmail.com>
2021-03-30 15:57:49 +0700

    sửa lại cho đàng hoàng

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Api quản lý, quả déo quả déo';
+    return 'Api quản lý cửa hàng Rangnar';
   }
 }
