5c267a768df5f2779d05a86e071905c01a1f0185
Nha Nguyen <nguyenphucnha111@gmail.com>
2021-03-31 08:32:11 +0700

    fix lỗi

--- src/chuong-trinh-khuyen-mai-value/chuong-trinh-khuyen-mai-value.module.ts
+++ src/chuong-trinh-khuyen-mai-value/chuong-trinh-khuyen-mai-value.module.ts
@@ -1,7 +1,7 @@
-import { ChuongTrinhKhuyenMaiValueController } from './chuong-trinh-khuyen-mai.controller';
+import { ChuongTrinhKhuyenMaiValueController } from './chuong-trinh-khuyen-mai-value.controller';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ChuongTrinhKhuyenMaiValueEntity } from './chuong-trinh-khuyen-mai.entity';
+import { ChuongTrinhKhuyenMaiValueEntity } from './chuong-trinh-khuyen-mai-value.entity';
 import { ChuongTrinhKhuyenMaiValueService } from './chuong-trinh-khuyen-mai-value.service';
 
 @Module({
