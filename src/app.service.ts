0d98f38697ed7fdba92b77352efc1e8077e6405a
nha1012 <46342451+nha1012@users.noreply.github.com>
2021-06-15 10:47:13 +0700

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Api quản lý cửa hàng Rangnar';
+    return 'Api quản lý cửa hàng thiết bị điện tử';
   }
 }
